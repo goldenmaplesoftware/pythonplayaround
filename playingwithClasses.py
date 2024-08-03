076a33a5b3319ec0a962d1816b8207effb383696
Jean Comtois <83982518+goldenmaplesoftware@users.noreply.github.com>
2024-08-03 16:03:56 -0400

    Sample list test
    sample list query

--- playingwithClasses.py
+++ playingwithClasses.py
@@ -24,27 +24,56 @@ class menu:
         
 
 # create class
-class GameMode:
+class GameModeBurger:
     name = "BLANK"
-
+    playerCash= 250000
+    inventory=[]
 
 class main:
     optionBeginGame=input("Type start to begin game, type end to end game\n")
     match optionBeginGame:
+    
     #1{  
       case "Start"|"start":
         print("Starting game:")
         playerName= input("Enter your name\n")
         print("Welcome to the restaurant business;",playerName)
         resturantType=input("Do you want to open a restaurant that serves burgers, or pizzas?\n")
+      
       #2{
         match resturantType:
           case "Burgers"|"burgers":
-            gameMode1 = GameMode()
+            gameMode1 = GameModeBurger()
           # modify the name property
             print("Before getting overwritten:", gameMode1.name)
             gameMode1.name = "This is the burger game mode test"
             print("After getting overwritten:", gameMode1.name)
+            print("Alright so you want to start a burger restaurant eh.\nWell",playerName,
+              ",you are going to have to have some capital to start out.\nHere is $",gameMode1.playerCash,"to start...")
+            print("You need to buy some inventory",gameMode1.inventory)
+            gameMode1.inventory.append("item 1")
+            print(gameMode1.inventory)
+            gameMode1.inventory.append("item 2")
+            print(gameMode1.inventory)
+            gameMode1.inventory.remove("item 1 ")
+            print(gameMode1.inventory)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           case "Pizzas"|"pizzas:":
             print("pizza option")
        #2}
